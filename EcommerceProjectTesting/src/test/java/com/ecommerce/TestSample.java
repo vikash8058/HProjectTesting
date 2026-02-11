16b453b148347aa22af6486a7f4a615624bfbaa4
Vikash Prajapati <vikashprajamdjssv@gmail.com>
2026-02-11 11:37:34 +0530

    [Vikash Prajapati] : added initial setup for ecommerce project testing

---
+++ EcommerceProjectTesting/src/test/java/com/ecommerce/TestSample.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+public class TestSample {
+	
+	@Test
+	public void testSample() {
+		Assert.assertEquals(3, 2+1);
+	}
+}
